54178cf81bab5238d4fa72ee681794bce6103cc3
apple115 <2567031030@qq.com>
2025-03-31 11:43:57 +0800

    refoctor: 优化

--- src/app/reader/page.tsx
+++ src/app/reader/page.tsx
@@ -2,7 +2,7 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { loadEpubMetaData } from "@/utils/epub";
-import ReaderComponent from "./ReaderComponent";
+import ReaderComponent from "@/app/reader/components/ReaderComponent";
 
 export default function ReaderPage() {
   const searchParams = useSearchParams();
